1141b46cd174c3f920d5beacffcb3ec35f5eebdb
Monica Pham <monica.hq.pham@gmail.com>
2023-06-01 17:50:18 -0400

    Completed Stupid Coach

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,20 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:question]
+    if @question.blank?
+      @answer = "I can't hear you!"
+    elsif @question =~ /i am going to work/i
+      @answer = "Great!"
+    elsif @question.ends_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
+
+
+# If the message is I am going to work, the coach will answer Great!
+# If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
+# Otherwise the coach will answer I don't care, get dressed and go to work!
